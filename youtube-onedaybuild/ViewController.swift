01783b93974d3d8538f7a6e3429909060fb0dbf3
DWGichuru <49855521+DWGichuru@users.noreply.github.com>
2023-04-15 14:46:14 -0700

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Duncan Gichuru on 2023-04-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
