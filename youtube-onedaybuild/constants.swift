d60798e013ab58f2ff7cb9d4153f1541dc2087ed
DWGichuru <49855521+DWGichuru@users.noreply.github.com>
2023-04-15 17:42:44 -0700

    added code to parse the json response

--- youtube-onedaybuild/constants.swift
+++ youtube-onedaybuild/constants.swift
@@ -10,6 +10,6 @@ import Foundation
 struct Constants {
     
     static var API_KEY = "AIzaSyDpvUDC8xdZABZzUU-YGWDFs9UNZdiyXfU"
-    static var PLAYLIST_ID = "PL8_LBdZbxgK3lmSzFoqaUrmcGMmt1sqHd"
+    static var PLAYLIST_ID = "PL8_LBdZbxgK0D3-gF1RyNi6E6yn_LiBkd"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
---
+++ youtube-onedaybuild/response.swift
@@ -0,0 +1,26 @@
+//
+//  response.swift
+//  youtube-onedaybuild
+//
+//  Created by Duncan Gichuru on 2023-04-15.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    
+    var items: [Video]?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case items
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+        
+    }
+}
